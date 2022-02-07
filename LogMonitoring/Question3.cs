2609fdc0c8bbe184c2360797ae4dcf5148e33535
MizukiEndo <99145768+MizukiEndo@users.noreply.github.com>
2022-02-07 13:58:50 +0900

    Add files via upload

--- LogMonitoring/Question3.cs
+++ LogMonitoring/Question3.cs
@@ -17,18 +17,18 @@ namespace LogMonitoring
 	internal class Question3
 	{
 		/// <summary>
-		/// チェック中データ保持クラス
+		/// サーバー毎のデータ保持するクラス
 		/// </summary>
-		private class CheckData
+		private class ServerData
 		{
 			public string ServerAddress = string.Empty;
 			public List<string[]> LogList = new List<string[]>();
 
 			/// <summary>
-			/// サーバー毎の情報を保持
+			/// 初回サーバー情報を保持
 			/// </summary>
 			/// <param name="values">ログ配列</param>
-			public CheckData(string[] values)
+			public ServerData(string[] values)
 			{
 				this.ServerAddress = values[Util.Cols.ServerAddress];
 				this.LogList.Add(values);
@@ -42,8 +42,8 @@ namespace LogMonitoring
 		/// <param name="vResponseBorder">平均応答時間の許容下限値</param>
 		public static void Answer(int vTargetCnt, int vResponseBorder)
 		{
-			//チェック中データリスト
-			List<CheckData> dataList = new List<CheckData>();
+			//サーバー毎のデータリスト
+			List<ServerData> serverList = new List<ServerData>();
 			using (StreamReader sr = new StreamReader(Util.TargetLogFileFullPath))
 			{
 				while (!sr.EndOfStream)
@@ -51,8 +51,8 @@ namespace LogMonitoring
 					string line = sr.ReadLine();
 					string[] values = line.Split(',');
 
-					//チェック中データリスト内の一致するインデックスを取得
-					int index = GetMatchIndexCheckDataList(dataList, values[Util.Cols.ServerAddress]);
+					//サーバー毎のデータリスト内の一致するインデックスを取得
+					int index = GetMatchIndexServerDataList(serverList, values[Util.Cols.ServerAddress]);
 
 					//応答なしのログは無視する
 					if (values[Util.Cols.ResponseResult].Equals("-"))
@@ -63,31 +63,31 @@ namespace LogMonitoring
 					if (index < 0)
 					{
 						//サーバーごとの初回処理
-						CheckData data = new CheckData(values);
-						dataList.Add(data);
+						ServerData data = new ServerData(values);
+						serverList.Add(data);
 						continue;
 					}
 					else
 					{
 						//サーバーごとのログを追加
-						dataList[index].LogList.Add(values);
+						serverList[index].LogList.Add(values);
 					}
 
-					if (dataList[index].LogList.Count > vTargetCnt)
+					if (serverList[index].LogList.Count > vTargetCnt)
 					{
 						//該当サーバーの監視対象ログが引数指定値より多い場合、古いログを削除
-						dataList[index].LogList.RemoveAt(0);
+						serverList[index].LogList.RemoveAt(0);
 					}
 
 					//監視対象ログが引数指定値より少ない場合は処理対象外
-					if (dataList[index].LogList.Count < vTargetCnt)
+					if (serverList[index].LogList.Count < vTargetCnt)
 					{
 						continue;
 					}
 
 					//応答時間の平均値算出
 					int res = 0;
-					foreach (string[] log in dataList[index].LogList)
+					foreach (string[] log in serverList[index].LogList)
 					{
 						res += Convert.ToInt32(log[Util.Cols.ResponseResult]);
 					}
@@ -96,7 +96,7 @@ namespace LogMonitoring
 					//応答時間平均値が引数指定値を超える場合、負荷状態として出力
 					if (ave > vResponseBorder)
 					{
-						Util.CalcDatetimeDiff dtInfo = new Util.CalcDatetimeDiff(dataList[index].LogList[0][Util.Cols.ChackDateTime], values[Util.Cols.ChackDateTime]);
+						Util.CalcDatetimeDiff dtInfo = new Util.CalcDatetimeDiff(serverList[index].LogList[0][Util.Cols.ChackDateTime], values[Util.Cols.ChackDateTime]);
 						StringBuilder sb = new StringBuilder();
 						sb.Append(values[Util.Cols.ServerAddress] + " ");
 						sb.Append(dtInfo.From + "～" + dtInfo.To);
@@ -108,16 +108,16 @@ namespace LogMonitoring
 		}
 
 		/// <summary>
-		/// チェック中データリスト内からサーバーアドレスが一致するインデックスを取得する
+		/// サーバー毎のデータリスト内からサーバーアドレスが一致するインデックスを取得する
 		/// </summary>
-		/// <param name="vCheckDataList">チェック中データリスト</param>
+		/// <param name="vServerDataList">サーバー毎のデータリスト</param>
 		/// <param name="vTargetServerAddress">対象サーバーアドレス</param>
 		/// <returns>一致するインデックス（存在しない場合は-1を返す）</returns>
-		private static int GetMatchIndexCheckDataList(List<CheckData> vCheckDataList, string vTargetServerAddress)
+		private static int GetMatchIndexServerDataList(List<ServerData> vServerList, string vTargetServerAddress)
 		{
-			for (int i = 0; i < vCheckDataList.Count; i++)
+			for (int i = 0; i < vServerList.Count; i++)
 			{
-				if (vCheckDataList[i].ServerAddress.Equals(vTargetServerAddress))
+				if (vServerList[i].ServerAddress.Equals(vTargetServerAddress))
 				{
 					return i;
 				}
